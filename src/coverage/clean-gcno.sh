64ae57c280389e10d4ff96da1ee96ae03730b335
Xavier MARCELET <xavier@marcelet.com>
2017-08-10 11:03:23 +0200

    fix symlink compatible cleaning

--- src/coverage/clean-gcno.sh
+++ src/coverage/clean-gcno.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 
-l_buildDir=$1
+l_buildDir="$1"
 
-l_files=$(find ${l_buildDir} -name '*.gcno')
+l_files=$(find -L "${l_buildDir}" -name '*.gcno')
 for c_file in $(echo ${l_files}); do
+  #echo "processing file : ${c_file}"
   l_items=$(gcov ${c_file} 2>/dev/null | grep File | sort | cut -d"'" -f2)
   l_found=1
   for c_item in $(echo ${l_items}); do
+    #echo "-> check item : ${c_item}"
     if [ ! -f ${c_item} ]; then
         l_found=0
     fi
